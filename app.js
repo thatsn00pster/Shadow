8b77b85ac83084c1d94cf08c7812f940ea70ceb3
thatsn00pster <168500351+thatsn00pster@users.noreply.github.com>
2026-01-14 06:57:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const Corrosion = require("./lib/server")
 const proxy = new Corrosion({
     prefix: config.prefix,
     codec: config.codec,
-    title: "Shadow",
+    title: "Moon",
     forceHttps: true,
     requestMiddleware: [
         Corrosion.middleware.blacklist([
@@ -53,5 +53,5 @@ app.use(function (req, res) {
 })
 
 app.listen(port, () => {
-    console.log(`Shadow is running at localhost:${port}`)
+    console.log(`Moon Unblock is running at localhost:${port}`)
 })
